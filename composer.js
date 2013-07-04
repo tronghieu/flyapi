54332fc2067cd365511bd04b448631c619aab620
Trong Hieu Luu <luutronghieu@alimama.vn>
2013-07-04 10:38:52 +0700

    Add composer.js

---
+++ composer.js
@@ -0,0 +1,15 @@
+{
+    "name": "tronghieu/FlyAPI",
+    "type": "library",
+    "description": "Flywheel API lib for server",
+    "keywords": ["framework", "API"],
+    "homepage": "http://luutronghieu.com",
+    "version" : "1.0",
+    "license": "MIT",
+    "authors": [
+        {
+            "name": "Luu Trong Hieu",
+            "email": "tronghieu.luu@gmail.com"
+        }
+    ]
+}
